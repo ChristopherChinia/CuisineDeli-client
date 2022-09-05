a8f0e0de4b17426bcc3987dcdf3403ef4c437277
Christopherchinia <chinia214@gmail.com>
2022-09-05 16:40:15 +0300

    adding a function to activate the light and dark mode feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import SideBar from "./components/SideBar";
+import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+  
   return (
     <div className="App">
       <div className="AppGlass">
         <BrowserRouter>
           <SideBar>
+          <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
             <Routes>
               {/* <Route path="/" element={<Home />} />
               <Route path="/" element={<Products />} />

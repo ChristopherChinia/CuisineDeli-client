b507bda75100b8c1a5c0c164b1e47eeef44760eb
Christopherchinia <chinia214@gmail.com>
2022-09-05 16:46:01 +0300

    creating a div class header for the icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './header.css';
+import { FaPizzaSlice,FaSearch, FaShoppingCart, FaUser, FaSun, FaMoon } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({ isDarkMode, onDarkModeClick }) => {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+    <h1> <FaPizzaSlice/> Cuisine delicacy</h1>
+    <div className="icons">
+      <ul>
+        <li><FaSearch /></li>
+        <li onClick={onDarkModeClick}>  {isDarkMode ? <FaMoon/> : <FaSun />}  </li>
+      </ul>
+    </div>
+  </div>
+);
 }
 
 export default Header

6dfec101400ea1578dbfaf63ad305a8d53b7f9fb
Christopherchinia <chinia214@gmail.com>
2022-09-05 19:24:52 +0300

    imported icons from react-icons/fa and bs library

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import './home.css';
+import {FaShippingFast, FaDollarSign, FaHeadset } from "react-icons/fa";
+import {BsEmojiHeartEyesFill, BsEmojiSunglasses, BsFillEmojiWinkFill} from "react-icons/bs";
 
 const Home = () =>{
     return(

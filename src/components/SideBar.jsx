586ad77497967e1613f6cb11b3a66fcb839a616f
Christopherchinia <chinia214@gmail.com>
2022-09-05 16:13:22 +0300

    adding variable with the paths for each route

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,6 +4,28 @@ import { FaTh,FaBars, FaUserAlt,FaCommentAlt, FaShoppingBag}from "react-icons/fa
 import { NavLink } from 'react-router-dom';
 
 const SideBar = () => {
+  const menuItem=[
+    {
+        path:"/",
+        name:"Home",
+        icon:<FaTh/>
+    },
+    {
+        path:"/product",
+        name:"Product",
+        icon:<FaShoppingBag/>
+    },
+    {
+        path:"/comment",
+        name:"Comment",
+        icon:<FaCommentAlt/>
+    },
+    {
+        path:"/about",
+        name:"About",
+        icon:<FaUserAlt/>
+    }
+]
   return (
     <div>SideBar</div>
   )

65a625cc1571504250d048165192730599f4cd50
Christopherchinia <chinia214@gmail.com>
2022-09-05 16:28:11 +0300

    creating various div containers and method on the sidbar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './sidebar.css';
-import { FaTh,FaBars, FaUserAlt,FaCommentAlt, FaShoppingBag}from "react-icons/fa";
+import { FaTh, FaBars, FaUserAlt, FaCommentAlt, FaShoppingBag}from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
 
-const SideBar = () => {
+const SideBar = ({extension}) => {
+  const[isOpen ,setIsOpen] = useState(false);
+  const toggle = () => setIsOpen (!isOpen);
   const menuItem=[
     {
         path:"/",
@@ -27,8 +29,26 @@ const SideBar = () => {
     }
 ]
   return (
-    <div>SideBar</div>
-  )
+<div className="container">
+           <div style={{width: isOpen ? "200px" : "50px"}} className="sidebar">
+               <div className="top_section">
+                   <h1 style={{display: isOpen ? "block" : "none"}} className="logo">Menu</h1>
+                   <div style={{marginLeft: isOpen ? "50px" : "0px"}} className="bars">
+                       <FaBars onClick={toggle}/>
+                   </div>
+               </div>
+               {
+                   menuItem.map((item, index)=>(
+                       <NavLink to={item.path} key={index} className="link" activeclassName="active">
+                           <div className="icon">{item.icon}</div>
+                           <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
+                       </NavLink>
+                   ))
+               }
+           </div>
+           <main>{extension}</main>
+        </div>
+    );
 }
 
 export default SideBar

9a8d5808dabb028c2d18c9dd6649347062847cd8
Christopherchinia <chinia214@gmail.com>
2022-09-05 15:00:27 +0300

    initialized my repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+       
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+

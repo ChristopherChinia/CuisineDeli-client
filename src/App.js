f36ddf597dbe2b782550a233047dde9655106c24
Christopherchinia <chinia214@gmail.com>
2022-09-05 15:32:39 +0300

    creating routes for home,products,comments,about in the app file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-import Sidebar from './components/Sidebar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-       
-        
+        <BrowserRouter>
+          <SideBar>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Products />} />
+              <Route path="/" element={<Comments />} />
+              <Route path="/" element={<About />} />
+            </Routes>
+          </SideBar>
+        </BrowserRouter>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
